a03bba1051d0a40cce9e92942be4c339c5e6e48d
tsuny221 <kingoftsuny@gmail.com>
2020-06-14 23:46:18 +0900

    [Finish] 2章

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Player
+{
+    private int hp = 100;
+    private int power = 50;
+
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    public void Damage(int damage)
+    {
+        this.hp -= damage;
+        Debug.Log(damage + "のダメージを受けた");
+    }
+}
 public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello, World!");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Player myPlayer = new Player();
+        myPlayer.Attack();
+        myPlayer.Damage(30);
     }
 }
